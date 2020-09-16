cfe45a1ee91bc20db8c55bc6f3c6e9ab92a77ddf
Maciej Treder <contact@maciejtreder.com>
2020-09-16 19:08:34 +0200

    header modifications

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,25 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
 
   @Input() displayTitle: boolean = false;
-
-  public title: Observable<string> = this.router.events.pipe(
-    filter(event => event instanceof NavigationEnd),
-    map((event: NavigationEnd) => event.urlAfterRedirects),
-    map(url => url=='/'?'/home':url),
-    map(url => url.substring(1))
-  );
-
-  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
