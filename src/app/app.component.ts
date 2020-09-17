18c8119e1751687c1f56e91c9953b9536f938a76
Maciej Treder <contact@maciejtreder.com>
2020-09-17 11:34:38 +0200

    navigate up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { distinctUntilChanged, filter, map } from 'rxjs/operators';
+import { BehaviorSubject, fromEvent, Observable, of, Subject } from 'rxjs';
+import { distinctUntilChanged, filter, map, mergeMap, switchMap } from 'rxjs/operators';
+import { NgAnimateScrollService } from 'ng-animate-scroll';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('state', [
+      state('visible', style({
+        opacity: 1,
+        'z-index': 1,
+      })),
+      state('unvisible', style({
+        opacity: 0,
+        'z-index': -1,
+      })),
+      transition('unvisible => visible', animate('500ms ease-in')),,
+      transition('visible => unvisible', animate('500ms ease-out')),
+    ])
+  ]
 })
 export class AppComponent {
+
   public displayMenu: Observable<boolean> = this.router.events.pipe(
     filter(event => event instanceof NavigationEnd),
     map((event: NavigationEnd) => event.urlAfterRedirects),
@@ -16,5 +33,23 @@ export class AppComponent {
     distinctUntilChanged()
   );
 
-  constructor(private router: Router){}
+  public scrollState: Subject<string> = new BehaviorSubject('unvisible');
+
+  constructor(private router: Router, private animateScrollService: NgAnimateScrollService){}
+
+  public navigateToHeader() {
+    this.animateScrollService.scrollToElement('header', 500)
+  }
+
+  ngOnInit() {
+    fromEvent(window, 'scroll').pipe(
+      map(() => window.pageYOffset > 0),
+      distinctUntilChanged(),
+      map(display => display?'visible':'unvisible')
+    ).subscribe(val => {
+      this.scrollState.next(val);
+    })
+
+    this.scrollState.subscribe(console.log)
+  }
 }
